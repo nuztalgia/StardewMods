9e9161236773457f09c2c220028b52d54abb9330
Nuz / Lovegood <nuztalgia@gmail.com>
2022-05-14 15:09:03 -0700

    Add skeletons for Dropdown and Image widgets.

---
+++ Common.UI/Widgets/Image.cs
@@ -0,0 +1,15 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Nuztalgia.StardewMods.Common.UI;
+
+internal class Image : Widget {
+
+  protected override void Draw(SpriteBatch sb, Vector2 position) {
+    throw new System.NotImplementedException();
+  }
+
+  protected override (int width, int height) UpdateDimensions(int totalWidth) {
+    throw new System.NotImplementedException();
+  }
+}
