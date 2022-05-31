57788915357472f0fab0ed53173500c8886a4580
Nuz / Lovegood <nuztalgia@gmail.com>
2022-05-31 14:27:26 -0700

    Un-nest some methods in ContentPatcherBridge for clarity.

--- DynamicMenuOptions/Framework/ContentPatcherBridge.cs
+++ DynamicMenuOptions/Framework/ContentPatcherBridge.cs
@@ -65,53 +65,54 @@ internal static class ContentPatcherBridge {
       ManagedConfigs =
           AllRawConfigs
               .Where(kvp => ManagedModIds.Contains(kvp.Key) && (kvp.Value != null))
-              .ToImmutableDictionary(kvp => kvp.Key, kvp => ParseModConfig(kvp.Key, kvp.Value!));
+              .ToImmutableDictionary(kvp => kvp.Key, kvp => ParseRawModConfig(kvp.Key, kvp.Value!));
       AllRawConfigs = null;
       OnContentPatcherInitialized?.Invoke();
     }
+  }
 
-    static IEnumerable<ConfigFieldData> ParseModConfig(string contentPackId, object rawConfigDict) {
-      Log.Trace($"Parsing config for registered content pack '{contentPackId}'.");
-      List<ConfigFieldData> configData = new();
+  private static IEnumerable<ConfigFieldData> ParseRawModConfig(
+      string contentPackId, object rawConfigDict) {
+    Log.Trace($"Parsing config for registered content pack '{contentPackId}'.");
+    List<ConfigFieldData> configData = new();
 
-      if (TryInvokeMethod(rawConfigDict, "get_Keys") is IEnumerable<string> configKeys) {
-        foreach (string configKey in configKeys) {
-          object?[] args = new object?[] { configKey, null };
-          if ((TryInvokeMethod(rawConfigDict, "TryGetValue", args) is true)
-              && (args[1] is object rawConfigField)) {
-            configData.Add(ParseConfigField(configKey, rawConfigField));
-          }
+    if (TryInvokeMethod(rawConfigDict, "get_Keys") is IEnumerable<string> configKeys) {
+      foreach (string configKey in configKeys) {
+        object?[] args = new object?[] { configKey, null };
+        if ((TryInvokeMethod(rawConfigDict, "TryGetValue", args) is true)
+            && (args[1] is object rawConfigField)) {
+          configData.Add(ParseRawConfigField(configKey, rawConfigField));
         }
       }
-      return configData;
+    }
+    return configData;
+  }
+
+  private static ConfigFieldData ParseRawConfigField(string configKey, object rawConfigField) {
+    (int? numericRangeMin, int? numericRangeMax) = (null, null);
+    object?[] args = new object?[] { null, null };
+
+    if (GetBoolean("IsNumericRange", args)) {
+      (numericRangeMin, numericRangeMax) = ((int?) args[0], (int?) args[1]);
     }
 
-    static ConfigFieldData ParseConfigField(string configKey, object rawConfigField) {
-      (int? numericRangeMin, int? numericRangeMax) = (null, null);
-      object?[] args = new object?[] { null, null };
+    return new ConfigFieldData(
+        key: configKey,
+        currentValues: GetEnumerable("get_Value"),
+        defaultValues: GetEnumerable("get_DefaultValues"),
+        allowedValues: GetEnumerable("get_AllowValues"),
+        allowBlankValues: GetBoolean("get_AllowBlank"),
+        allowMultipleValues: GetBoolean("get_AllowMultiple"),
+        isBooleanValue: GetBoolean("IsBoolean"),
+        numericRangeMin: numericRangeMin,
+        numericRangeMax: numericRangeMax);
 
-      if (GetBoolean("IsNumericRange", args)) {
-        (numericRangeMin, numericRangeMax) = ((int?) args[0], (int?) args[1]);
-      }
+    IEnumerable<string>? GetEnumerable(string methodName) {
+      return TryInvokeMethod(rawConfigField, methodName) as IEnumerable<string>;
+    }
 
-      return new ConfigFieldData(
-          key: configKey,
-          currentValues: GetEnumerable("get_Value"),
-          defaultValues: GetEnumerable("get_DefaultValues"),
-          allowedValues: GetEnumerable("get_AllowValues"),
-          allowBlankValues: GetBoolean("get_AllowBlank"),
-          allowMultipleValues: GetBoolean("get_AllowMultiple"),
-          isBooleanValue: GetBoolean("IsBoolean"),
-          numericRangeMin: numericRangeMin,
-          numericRangeMax: numericRangeMax);
-
-      IEnumerable<string>? GetEnumerable(string methodName) {
-        return TryInvokeMethod(rawConfigField, methodName) as IEnumerable<string>;
-      }
-
-      bool GetBoolean(string methodName, object?[]? args = null) {
-        return TryInvokeMethod(rawConfigField, methodName, args) is true;
-      }
+    bool GetBoolean(string methodName, object?[]? args = null) {
+      return TryInvokeMethod(rawConfigField, methodName, args) is true;
     }
   }
 
